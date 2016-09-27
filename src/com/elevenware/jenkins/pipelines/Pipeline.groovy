288dcd263ee002ed6e4d7f4a768594be431a7a24
George McIntosh <george@elevenware.com>
2016-09-27 18:21:34 +0100

    refactor

--- src/com/elevenware/jenkins/pipelines/Pipeline.groovy
+++ src/com/elevenware/jenkins/pipelines/Pipeline.groovy
@@ -3,9 +3,7 @@ package com.elevenware.jenkins.pipelines
 //class Pipeline implements Serializable {
 
     void run(object) {
-        println "hello there $object"
-        object.echo("OBJECT ${object.getClass().canonicalName}")
-        object.stage(name: 'qa') {
+       stage(name: 'qa') {
             echo "runing stage QA"
         }
     }

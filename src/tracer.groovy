4dbf461bf74670a5aaa56a4176dc6063daed7a1f
George McIntosh <george@elevenware.com>
2016-09-27 15:50:46 +0100

    more

--- src/tracer.groovy
+++ src/tracer.groovy
@@ -1,7 +1,9 @@
-def call(body) {
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
-    echo "RUNNING"
+{ ->
+
+    hello() {
+        echo "hello"
+    }
+
 }
+
+this

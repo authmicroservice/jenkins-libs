dc391ce42b284ab1c6cb6e6d8bb0d06c482b03f4
George McIntosh <george@elevenware.com>
2016-09-28 09:10:51 +0100

    refactor

--- src/com/elevenware/jenkins/pipelines/SimpleStage.groovy
+++ src/com/elevenware/jenkins/pipelines/SimpleStage.groovy
@@ -1,6 +1,7 @@
 package com.elevenware.jenkins.pipelines
 
 def create(String name, String env) {
-
-    echo "Running ${env} for ${name} app"
+    stage(name: env) {
+        echo "Running ${env} for ${name} app"
+    }
 }

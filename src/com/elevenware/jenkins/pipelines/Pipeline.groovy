375532cf539694dfdd4596107dc4b1185d1ef913
George McIntosh <george@elevenware.com>
2016-09-28 09:06:38 +0100

    refactor

--- src/com/elevenware/jenkins/pipelines/Pipeline.groovy
+++ src/com/elevenware/jenkins/pipelines/Pipeline.groovy
@@ -7,7 +7,7 @@ package com.elevenware.jenkins.pipelines
     void generate() {
 
         def environments = ['integration', 'qa', 'staging', 'production']
-        
+
         environments.each { env ->
             new SimpleStage().create('test app', env)
         }
--- src/com/elevenware/jenkins/pipelines/SimpleStage.groovy
+++ src/com/elevenware/jenkins/pipelines/SimpleStage.groovy
@@ -1,5 +1,6 @@
 package com.elevenware.jenkins.pipelines
 
 def create(String name, String env) {
+
     echo "Running ${env} for ${name} app"
 }

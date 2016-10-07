c5e97fd396262ea2067863943dd90cca423242a3
George McIntosh <george@elevenware.com>
2016-10-07 11:47:02 +0100

    spiking

--- src/com/elevenware/jenkins/pipelines/Pipeline.groovy
+++ src/com/elevenware/jenkins/pipelines/Pipeline.groovy
@@ -10,7 +10,7 @@ class Pipeline {
 
     private static void ensurePopulated() {
         if(!TYPES.isEmpty()) { return }
-        TYPES.put('github', GithubPipeline)
+        TYPES.put('github', GithubPipeline.class)
     }
 
 }

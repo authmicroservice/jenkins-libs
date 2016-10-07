853a45bc601fd6a54eedc1ff0fe88f5898571e6a
George McIntosh <george@elevenware.com>
2016-10-07 10:05:27 +0100

    spiking

--- vars/pipeline.groovy
+++ vars/pipeline.groovy
@@ -3,7 +3,7 @@ import com.elevenware.jenkins.pipelines.Pipeline
 import com.elevenware.jenkins.pipelines.PipelineBuilderDelegate
 
 def call(Closure body) {
-    properties [[$class: 'BuildDiscarderProperty', strategy: [$class: 'LogRotator', artifactDaysToKeepStr: '20', artifactNumToKeepStr: '30', daysToKeepStr: '20', numToKeepStr: '30']]]
+//    properties [[$class: 'BuildDiscarderProperty', strategy: [$class: 'LogRotator', artifactDaysToKeepStr: '20', artifactNumToKeepStr: '30', daysToKeepStr: '20', numToKeepStr: '30']]]
     PipelineBuilder builder = new PipelineBuilder()
     body.setDelegate(new PipelineBuilderDelegate(builder))
     body.setResolveStrategy(Closure.DELEGATE_FIRST)

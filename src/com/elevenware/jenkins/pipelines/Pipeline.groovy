50a380e6f9dd08b397d092f5e2a18ce5d341d841
George McIntosh <george@elevenware.com>
2016-10-07 11:49:37 +0100

    spiking

--- src/com/elevenware/jenkins/pipelines/Pipeline.groovy
+++ src/com/elevenware/jenkins/pipelines/Pipeline.groovy
@@ -2,15 +2,15 @@ package com.elevenware.jenkins.pipelines
 
 class Pipeline {
 
-    private static Map TYPES = [:]
+    private static Map TYPES = [github: GithubPipeline]
 
     static Pipeline forType(String type) {
 
     }
-
-    private static void ensurePopulated() {
-        if(!TYPES.isEmpty()) { return }
-        TYPES.put('github', GithubPipeline)
-    }
+//
+//    private static void ensurePopulated() {
+//        if(!TYPES.isEmpty()) { return }
+//        TYPES.put('github', GithubPipeline)
+//    }
 
 }

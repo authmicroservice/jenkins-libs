24074afaf9b8951c064ad461185994e1f963b5b5
George McIntosh <george@elevenware.com>
2016-10-07 10:38:01 +0100

    spiking

--- src/com/elevenware/jenkins/pipelines/Pipeline.groovy
+++ src/com/elevenware/jenkins/pipelines/Pipeline.groovy
@@ -1,7 +1,9 @@
 package com.elevenware.jenkins.pipelines
 
-/**
- * Created by george on 07/10/2016.
- */
-class Pipeline {
+abstract class Pipeline {
+
+    static Pipeline forType(String type) {
+        
+    }
+
 }
--- src/com/elevenware/jenkins/pipelines/PipelineBuilder.groovy
+++ src/com/elevenware/jenkins/pipelines/PipelineBuilder.groovy
@@ -2,14 +2,14 @@ package com.elevenware.jenkins.pipelines
 
 class PipelineBuilder implements Serializable {
 
-//    private Pipeline myPipeline
+    private Pipeline myPipeline
 
     void createFlow(String flowType) {
-//        this.myPipeline = Pipeline.forType(flowType)
+        this.myPipeline = Pipeline.forType(flowType)
     }
 
     Pipeline getPipeline() {
-        
+
     }
 
 }

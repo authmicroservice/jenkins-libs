eae7e8dcf7f6af5f6398c17fad0d6750cc5472ef
George McIntosh <george@elevenware.com>
2016-10-07 15:35:52 +0100

    spiking

--- src/com/elevenware/jenkins/pipelines/elements/BuildTestPublishElement.groovy
+++ src/com/elevenware/jenkins/pipelines/elements/BuildTestPublishElement.groovy
@@ -1,12 +1,14 @@
 package com.elevenware.jenkins.pipelines.elements
 
 class BuildTestPublishElement extends PipelineElement {
+
     @Override
     void generate(steps) {
         steps.node {
             steps.stage("Build-Test-Publish") {
-                steps.echo "Hello, world"
+                steps.echo "Hello, world ${steps.getClass()}"
             }
         }
     }
+
 }

fbbe794d971484a9a161a82617e379fee5026699
George McIntosh <george@elevenware.com>
2016-10-07 15:40:18 +0100

    spiking

--- src/com/elevenware/jenkins/pipelines/elements/BuildTestPublishElement.groovy
+++ src/com/elevenware/jenkins/pipelines/elements/BuildTestPublishElement.groovy
@@ -3,12 +3,13 @@ package com.elevenware.jenkins.pipelines.elements
 class BuildTestPublishElement extends PipelineElement {
 
     @Override
-    void generate(steps) {
-        steps.node {
-            steps.stage("Build-Test-Publish") {
-                steps.echo "Hello, world ${steps.getClass()}"
+    Closure getDefinition() {
+       return {
+            node {
+                stage("Build-Test-Publish") {
+                   echo("Hello, world!")
+                }
             }
         }
     }
-
 }
--- src/com/elevenware/jenkins/pipelines/elements/PipelineElement.groovy
+++ src/com/elevenware/jenkins/pipelines/elements/PipelineElement.groovy
@@ -3,6 +3,13 @@ package com.elevenware.jenkins.pipelines.elements
 abstract class PipelineElement implements Serializable {
 
     @NonCPS
-    abstract void generate(steps)
+    void generate(steps) {
+        Closure closure = getDefinition()
+        closure.setDelegate(steps)
+        closure.call()
+    }
+
+    @NonCPS
+    abstract Closure getDefinition()
 
 }

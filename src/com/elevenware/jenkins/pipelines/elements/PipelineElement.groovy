a13a23545f6c7d23fa143d522d4c207512914b90
George McIntosh <george@elevenware.com>
2016-10-07 15:30:17 +0100

    spiking

--- src/com/elevenware/jenkins/pipelines/elements/PipelineElement.groovy
+++ src/com/elevenware/jenkins/pipelines/elements/PipelineElement.groovy
@@ -1,6 +1,6 @@
 package com.elevenware.jenkins.pipelines.elements
 
-abstract class PipelineElement {
+abstract class PipelineElement implements Serializable {
 
     @NonCPS
     abstract void generate(steps)

b74241afcd3cd19c4ac82adb95a700cf37ee1672
George McIntosh <george@elevenware.com>
2016-10-07 10:40:57 +0100

    spiking

--- src/com/elevenware/jenkins/pipelines/Pipeline.groovy
+++ src/com/elevenware/jenkins/pipelines/Pipeline.groovy
@@ -2,16 +2,16 @@ package com.elevenware.jenkins.pipelines
 
 abstract class Pipeline {
 
-    private static Map TYPES = [
-            'github': GithubPipeline
-    ]
-
-    static Pipeline forType(String type) {
-        Class pipelineClass = TYPES[type]
-        if(!pipelineClass) {
-            throw new RuntimeException("No defined flow for $type")
-        }
-        pipelineClass.newInstance()
-    }
+//    private static Map TYPES = [
+//            'github': GithubPipeline
+//    ]
+//
+//    static Pipeline forType(String type) {
+//        Class pipelineClass = TYPES[type]
+//        if(!pipelineClass) {
+//            throw new RuntimeException("No defined flow for $type")
+//        }
+//        pipelineClass.newInstance()
+//    }
 
 }
--- vars/pipeline.groovy
+++ vars/pipeline.groovy
@@ -4,7 +4,7 @@ import com.elevenware.jenkins.pipelines.PipelineBuilderDelegate
 
 
 def call(Closure body) {
-//    properties [[$class: 'BuildDiscarderProperty', strategy: [$class: 'LogRotator', artifactDaysToKeepStr: '20', artifactNumToKeepStr: '30', daysToKeepStr: '20', numToKeepStr: '30']]]
+    properties [[$class: 'BuildDiscarderProperty', strategy: [$class: 'LogRotator', artifactDaysToKeepStr: '20', artifactNumToKeepStr: '10', daysToKeepStr: '20', numToKeepStr: '10']]]
     PipelineBuilder builder = new PipelineBuilder()
     body.setDelegate(new PipelineBuilderDelegate(builder))
     body.setResolveStrategy(Closure.DELEGATE_FIRST)

627f31d7c2b4f3ea1f5c5125495b6b25247cf9b9
George McIntosh <george@elevenware.com>
2016-10-07 10:45:20 +0100

    spiking

--- src/com/elevenware/jenkins/pipelines/Pipeline.groovy
+++ src/com/elevenware/jenkins/pipelines/Pipeline.groovy
@@ -2,16 +2,5 @@ package com.elevenware.jenkins.pipelines
 
 class Pipeline {
 
-//    private static Map TYPES = [
-//            'github': GithubPipeline
-//    ]
-//
-//    static Pipeline forType(String type) {
-//        Class pipelineClass = TYPES[type]
-//        if(!pipelineClass) {
-//            throw new RuntimeException("No defined flow for $type")
-//        }
-//        pipelineClass.newInstance()
-//    }
 
 }

dc847f6a8e4972ae3f4761330d5dc348dd74e8d8
George McIntosh <george@elevenware.com>
2016-10-07 11:25:24 +0100

    spiking

--- src/com/elevenware/jenkins/pipelines/Pipeline.groovy
+++ src/com/elevenware/jenkins/pipelines/Pipeline.groovy
@@ -1,12 +1,11 @@
 package com.elevenware.jenkins.pipelines
 
-/**
- * Created by george on 07/10/2016.
- */
 class Pipeline {
 
+    private static Map TYPES = [:]
+
     static Pipeline forType(String type) {
-        
+
     }
 
 }

5355d1cd3e7cbde85b1f5570053ac33c6a2db31c
George McIntosh <george@elevenware.com>
2016-10-10 12:16:19 +0100

    spiking

--- src/com/elevenware/jenkins/pipelines/JavaPlatform.groovy
+++ src/com/elevenware/jenkins/pipelines/JavaPlatform.groovy
@@ -3,5 +3,18 @@ package com.elevenware.jenkins.pipelines
 class JavaPlatform extends Platform {
 
 
+    @Override
+    void build() {
 
+    }
+
+    @Override
+    void test() {
+
+    }
+
+    @Override
+    void publish() {
+
+    }
 }

c2a87fa82619ab27aa9deb9eadca1ad99fc11b93
George McIntosh <george@elevenware.com>
2016-11-28 11:29:50 +0000

    adding to pipeline

--- src/com/elevenware/jenkins/pipelines/Pipeline.groovy
+++ src/com/elevenware/jenkins/pipelines/Pipeline.groovy
@@ -22,7 +22,6 @@ abstract class Pipeline implements Serializable {
 
 //        List elements = getElements()
         PipelineElement start = getStart()
-        throw new RuntimeException("FPIND $start")
         start.doGenerate(this.platform)
 //        for(int i = 0; i < elements.size(); i++) {
 //            PipelineElement element = elements.get(i)

0146e459774825d61c29e695f5f0036555d17530
George McIntosh <george@elevenware.com>
2016-12-01 10:52:06 +0000

    change

--- src/com/elevenware/jenkins/pipelines/functions/Deployments.groovy
+++ src/com/elevenware/jenkins/pipelines/functions/Deployments.groovy
@@ -1,13 +1,18 @@
 package com.elevenware.jenkins.pipelines.functions
 
-def deploy(String env, Map config) {
-    def role = config.role
-    node {
-        String rootDeployScript = libraryResource 'scripts/tracer.sh'
-        stage("deploy-${role}-${env}") {
-            echo "Deploying t${role} o ${env}"
-            def deployScript = rootDeployScript.replace('${ENVIRONMENT}', env)
-            sh "${deployScript}"
+class Deployments {
+
+    def deploy(String env, Map config) {
+        def role = config.role
+        node {
+            String rootDeployScript = libraryResource 'scripts/tracer.sh'
+            stage("deploy-${role}-${env}") {
+                echo "Deploying t${role} o ${env}"
+                def deployScript = rootDeployScript.replace('${ENVIRONMENT}', env)
+                sh "${deployScript}"
+            }
         }
     }
+
 }
+

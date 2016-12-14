ab55547a68ea2ba00e78d83fb56fd0a1ae5e5860
George McIntosh <george@elevenware.com>
2016-12-14 12:37:06 +0000

    change

--- src/com/elevenware/jenkins/pipelines/definitions/ChefSteps.groovy
+++ src/com/elevenware/jenkins/pipelines/definitions/ChefSteps.groovy
@@ -2,7 +2,7 @@ package com.elevenware.jenkins.pipelines.definitions
 
 def pinEnvironment() {
     String script = libraryResource('scripts/chef/environmentPin.sh')
-    script.replace('${ROLE}', 'foo')
+    script = script.replace('${ROLE}', 'foo')
     echo "Script is ${script}"
     sh script
 }

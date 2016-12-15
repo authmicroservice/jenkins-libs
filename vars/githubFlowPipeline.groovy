e25f3c99886ea4933f16bccca35865c6ccc3829d
George McIntosh <george@elevenware.com>
2016-12-15 16:41:19 +0000

    change

--- vars/githubFlowPipeline.groovy
+++ vars/githubFlowPipeline.groovy
@@ -19,7 +19,7 @@ def call(Closure body) {
 // short SHA, possibly better for chat notifications, etc.
             shortCommit = gitCommit.take(8)
             config['version'] = shortCommit
-            echo "BULD ${currentBuild}"
+            echo "BULD ${currentBuild.externalizableId}"
         }
         stage("build-${config.appName}") {
             githubFlow.buildAndPublishArtifact(config)

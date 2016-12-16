4ae30a067dcfbe85040cd907f858e298982ec8e8
George McIntosh <george@elevenware.com>
2016-12-16 10:05:14 +0000

    change

--- src/com/elevenware/jenkins/pipelines/definitions/ChefSteps.groovy
+++ src/com/elevenware/jenkins/pipelines/definitions/ChefSteps.groovy
@@ -1,8 +1,15 @@
 package com.elevenware.jenkins.pipelines.definitions
 
 def pinEnvironment(Map config) {
+    loadChefBundle()
     echo "VERSION = ${config.version}"
     String script = libraryResource('scripts/chef/environmentPin.sh')
     script = script.replace('${ROLE}', 'foo')
     sh script
+}
+
+def loadChefBundle() {
+    String gemfile = libraryResource 'scripts/chef/chef_gemfile'
+    sh "cat ${gemfile} > Gemfile"
+    sh 'ls'
 }

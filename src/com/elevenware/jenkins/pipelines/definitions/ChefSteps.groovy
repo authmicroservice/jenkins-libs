d9e9a9c943748939ac6d0af3237ea088e5c09986
George McIntosh <george@elevenware.com>
2016-12-16 10:28:39 +0000

    change

--- src/com/elevenware/jenkins/pipelines/definitions/ChefSteps.groovy
+++ src/com/elevenware/jenkins/pipelines/definitions/ChefSteps.groovy
@@ -12,6 +12,7 @@ def pinEnvironment(Map config) {
 
 def loadChefBundle() {
     String gemfileContent = libraryResource 'scripts/chef/chef_gemfile'
+    echo currentBuild.buildVariables
     if(currentBuild.workspace.isRemote())
     {
         def channel = currentBuild.workspace.channel;

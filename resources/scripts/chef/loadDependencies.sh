41e53fb4314fed6918a0dcee8d0abf71b3ed0ed4
George McIntosh <george@elevenware.com>
2017-01-04 10:52:31 +0000

    change

--- resources/scripts/chef/loadDependencies.sh
+++ resources/scripts/chef/loadDependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Loading chef dependencies"
-export PATH=$PATH:/var/lib/jenkins/.rvm/rubies/ruby-1.9.3-p551/bin
+export PATH=$PATH:/var/lib/jenkins/.rvm/rubies/ruby-2.2.6/bin
 ruby -v
 
 gem install bundler

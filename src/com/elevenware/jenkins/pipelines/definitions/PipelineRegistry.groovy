6c8f17c36d1a9e4516340bcc34ba9e549cda9cea
George McIntosh <george@elevenware.com>
2017-01-18 16:15:58 +0000

    make registry serializable

--- src/com/elevenware/jenkins/pipelines/definitions/PipelineRegistry.groovy
+++ src/com/elevenware/jenkins/pipelines/definitions/PipelineRegistry.groovy
@@ -4,7 +4,7 @@ class PipelineRegistry implements Serializable {
 
     private static PipelineRegistry INSTANCE = new PipelineRegistry()
 
-    private def registry = [:]
+    private registry = [:]
 
     private PipelineRegistry() {
         registry['githubflow'] = GithubPipelineDefinition
@@ -15,7 +15,7 @@ class PipelineRegistry implements Serializable {
     }
 
     def create(String name) {
-        if(!registry.containsValue()) {
+        if(!registry.containsKey(name)) {
             throw new RuntimeException("Cannot find pipeline type '${name}' in '${registry.keySet().join(' ')}'")
         }
         Class pipelineClass = registry[name]

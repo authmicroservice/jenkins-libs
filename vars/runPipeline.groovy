e184a110767296d25c5d8de6c8c73c0b9876d120
George McIntosh <george@elevenware.com>
2017-01-23 11:29:52 +0000

    PipelineContext constructor is now no-args

--- vars/runPipeline.groovy
+++ vars/runPipeline.groovy
@@ -5,7 +5,7 @@ def call(String pipelineName, Closure body) {
 
     def pipelineDef = PipelineRegistry.instance.create(pipelineName)
 
-    PipelineContext ctx = new PipelineContext(pipelineName)
+    PipelineContext ctx = new PipelineContext()
     body.delegate = ctx
     body.resolveStrategy = Closure.DELEGATE_ONLY
     body()

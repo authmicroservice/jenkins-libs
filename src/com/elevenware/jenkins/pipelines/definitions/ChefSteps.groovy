c58316d7201bec8cabfc69434a048de26a10ecc4
George McIntosh <george@elevenware.com>
2017-02-16 13:36:49 +0000

    EVERYTHING has to be serializable apparently

--- src/com/elevenware/jenkins/pipelines/definitions/ChefSteps.groovy
+++ src/com/elevenware/jenkins/pipelines/definitions/ChefSteps.groovy
@@ -4,6 +4,7 @@ import com.elevenware.jenkins.pipelines.PipelineContext
 
 def installChefDependencies(PipelineContext ctx) {
     echo'Installing cookbook dependencies'
+    echo "I AM IN ${env.WORKSPACE}"
     dir ctx.cookbookDir
     sh "bundle install --path \"~/.gem\""
 }
